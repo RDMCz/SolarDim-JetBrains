f956460050958ee6431fab758e0fae5e693794a0
RDMCz <dadun001@email.cz>
2025-10-31 00:24:12 +0100

    Proper SolarDim Rust (tested in RustRover)

--- src/build.py
+++ src/build.py
@@ -8,10 +8,12 @@ PATH_THEME = PATH / ".." / "SolarDim" / "resources" / "theme"
 
 
 def main():
+    # Load colors from `colors.json` into dictionary
     with open(PATH / "colors.json", "r", encoding="utf-8") as f_in:
         colors = json.load(f_in)
 
     # ---
+    # Replace all {{colorname}} in `SolarDim.theme.json` and save it in `SolarDim/resources/theme`
 
     with open(PATH / "SolarDim.theme.json", "r", encoding="utf-8") as f_in:
         theme = f_in.read()
@@ -21,6 +23,7 @@ def main():
         if key in colors:
             return colors[key]
         print(f"[!] Key {key} does not exist.", file=sys.stderr)
+        input()
         return "#000000"
 
     result = re.sub(r"{{(\w+)}}", replacer, theme)
@@ -30,6 +33,7 @@ def main():
         json.dump(result_dict, f_out, indent=4, ensure_ascii=False)
 
     # ---
+    # Replace all {{colorname}} in `scheme.xml` (this time color codes are without `#`) and save it in `SolarDim/resources/theme`
 
     with open(PATH / "scheme.xml", "r", encoding="utf-8") as f_in:
         scheme = f_in.read()
@@ -39,6 +43,7 @@ def main():
         if key in colors:
             return colors[key][1:]
         print(f"[!] Key {key} does not exist.", file=sys.stderr)
+        input()
         return "000000"
 
     result = re.sub(r"{{(\w+)}}", replacer2, scheme)
@@ -48,5 +53,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main()    
     print("ok")
